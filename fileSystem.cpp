7a423eefe5257a671e6a882f0b2c8b6b9b46fdbc
Mutasim <95741325+meowtasim@users.noreply.github.com>
2023-07-26 14:29:50 +0530

    Update fileSystem.cpp

--- fileSystem.cpp
+++ fileSystem.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <string>
-#define diskSpace 1024//in terms of mb
-#define blockSize 1//in terms of mb
+#define diskSpace 1024 // in terms of mb
+#define blockSize 1    // in terms of mb
+int numberOfBlocks = diskSpace / blockSize;
 using namespace std;
-typedef struct fileList
-{
-    file *currentFile;
-    fileNodes *next;
-} fileNodes; // A linked list used to keep track of all files in a directory
-// typedef struct linkedlist{
-//     int pageNumber;
-//     linkedList* next;
-// }linkedList;//For file allocation table
+
+// typedef struct fileList
+// {
+//     file *currentFile;
+//     fileNodes *next;
+// } fileNodes; // A linked list used to keep track of all files in a directory
+
 class directory
 {
 private:
@@ -20,10 +19,9 @@ private:
     string creationDate;
     int numberOfFiles;
     int directorySize;
-    
+
 public:
     // Operations allowed on directories
-
 };
 class file : directory
 {
@@ -33,7 +31,7 @@ private:
     string creationDate;
     int fileSize;
     string dataType;
-    //permissions
+    // permissions
     int readFile;
     int writeFile;
     int executeFile;
@@ -41,18 +39,34 @@ private:
     int closeFile;
     int renameFile;
     int deleteFile;
+
 public:
     // Operations allowed on files
-    file(int a=0){//0 means public user and 1 means owner
-        //set up permissions for public and owner
+    file(int a = 0)
+    { // 0 means public user and 1 means owner
+      // set up permissions for public and owner
     }
 };
+int findEmptyBlock(){
+    for(int i=1;i<numberOfBlocks;i++){
+        if()
+    }
+}
+file *createFile(string fileContent)
+{
+    int fileSize=sizeof(fileContent);
+    while (fileSize > blockSize)
+    {
+        findEmptyBlock;
+
+    }
+}
 int main()
 {
-    int numberOfBlocks=diskSpace/blockSize;
-    int FAT[numberOfBlocks];//If an entry has value 0, that block is unused
-    //initialize the FAT with all 0s representing empty blocks
-    for(int i=0;i<numberOfBlocks;i++){
-        FAT[i]=0;
+    int FAT[numberOfBlocks]; // If an entry has value 0, that block is unused
+    // initialize the FAT with all 0s representing empty blocks
+    for (int i = 0; i < numberOfBlocks; i++)
+    {
+        FAT[i] = 0;
     }
 }
