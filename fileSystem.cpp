6e3de713585a85d1548e5514c1fbfea824e43a9e
Mutasim <95741325+meowtasim@users.noreply.github.com>
2023-07-26 13:06:22 +0530

    Update fileSystem.cpp

--- fileSystem.cpp
+++ fileSystem.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <string>
+#define diskSpace 1024//in terms of mb
+#define blockSize 1//in terms of mb
 using namespace std;
 typedef struct fileList
 {
     file *currentFile;
     fileNodes *next;
 } fileNodes; // A linked list used to keep track of all files in a directory
+// typedef struct linkedlist{
+//     int pageNumber;
+//     linkedList* next;
+// }linkedList;//For file allocation table
 class directory
 {
 private:
@@ -43,4 +49,10 @@ public:
 };
 int main()
 {
+    int numberOfBlocks=diskSpace/blockSize;
+    int FAT[numberOfBlocks];//If an entry has value 0, that block is unused
+    //initialize the FAT with all 0s representing empty blocks
+    for(int i=0;i<numberOfBlocks;i++){
+        FAT[i]=0;
+    }
 }
