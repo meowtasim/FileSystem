d3039ac62328c31cd1b3d56476e81b82a354000f
Mutasim <95741325+meowtasim@users.noreply.github.com>
2023-07-27 12:55:55 +0530

    Update fileSystem.cpp

--- fileSystem.cpp
+++ fileSystem.cpp
@@ -23,6 +23,7 @@ public:
 allocationTableBlock FAT[numberOfBlocks];
 // Find free blocks in constant time
 class file;
+class directory;
 class directory
 {
 private:
@@ -31,13 +32,20 @@ private:
     string creationDate;
     int directorySize;
     // Tabel that points to all file objects, also point towards directory objects
-    file *fileRecords = NULL; // for a single level directory
+    file *fileRecords; // for a single level directory
     int numberOfFiles = 0;
-    directory *directoryRecords = NULL;
+    directory *directoryRecords;
     int numberOfDirectories = 0;
 
 public:
     // Operations allowed on directories
+    directory()
+    {
+        fileRecords = new file[20];
+        directoryRecords = new directory[5];
+    }
+    friend void removeFileFromDirectory(file &a);
+    friend void moveFile(file &a, directory &d);
 };
 class file
 {
@@ -76,10 +84,12 @@ public: // Need to set default values
     }
     friend void deleteFile(file &a);
     friend void createFile(char fileContent[], file &actualFile);
+    friend void removeFileFromDirectory(file &a);
+    friend void moveFile(file &a, directory &d);
 };
 int findEmptyBlock() // need to make it constant time
 {
-    for (int i = 1; i < numberOfBlocks; i++)
+    for (int i = 0; i < numberOfBlocks; i++)
     {
         if (FAT[i].used == 0)
         {
@@ -89,6 +99,7 @@ int findEmptyBlock() // need to make it constant time
 } // Returns the index of the first empty block
 void takeFileMetadata(file &actualFile)
 {
+
     // need to take file name and everything from user and time
 } // |1| |2| |3| |4| |5|
 void createFile(char fileContent[], file &actualFile) // Need to update directory
@@ -121,40 +132,62 @@ void createFile(char fileContent[], file &actualFile) // Need to update director
     cout << "Finishing file creation" << endl;
 }
 
-//Function to remove file from directory array
-void removeFileFromDirectory(file &a){
-
-}
+// Function to remove file from directory array
+void removeFileFromDirectory(file &a)
+{
+    int filePosition;
+    for (int filePosition = 0; filePosition < a.blocksUsed; filePosition++)// find file position in file list
+    {
+        if (a.name == a.locationDirectory->fileRecords[filePosition].name)
+        { 
+            break;
+        }
+    } 
+    for (int i = filePosition; i < a.locationDirectory->numberOfFiles; i++)// shifting remaining entries
+    {
+        a.locationDirectory->fileRecords[i] = a.locationDirectory->fileRecords[i + 1]; // shift by 1
+        a.locationDirectory->numberOfFiles--;
+    }
+} // Succesfully removed file from directory
 
 // createDirectory funtion
 // delete file
-void deleteFile(file &a)//done
+void deleteFile(file &a) // done
 {
     removeFileFromDirectory(a);
-    for(int i=0;i<a.blocksUsed;i++){
-        FAT[a.indexTable[i]].used=0;//Mark block as unused
-        delete FAT[a.indexTable[i]].blockPointer;//free memory space allocated for block
-        FAT[a.indexTable[i]].blockPointer=NULL;
+    for (int i = 0; i < a.blocksUsed; i++)
+    {
+        FAT[a.indexTable[i]].used = 0;            // Mark block as unused
+        delete FAT[a.indexTable[i]].blockPointer; // free memory space allocated for block
+        FAT[a.indexTable[i]].blockPointer = NULL;
     }
-    a.~file();//destroy the file object hopefully
+    a.~file(); // destroy the file object hopefully
 }
 // delete directory
 // rename
 // moving
-void moveFile(file &a,directory &d){//move file to destination directory
-    
+void moveFile(file &a, directory &d)
+{ // move file to destination directory
+    if (d.numberOfFiles == 20)
+    { // if space is not available
+        cout << "Error : Maximum capacity of directory reached" << endl;
+        return;
+    }
+    d.numberOfFiles++;
+    d.fileRecords[d.numberOfFiles] = a; // first add file to new directory
+    removeFileFromDirectory(a);         // Remove file from previous directory
+    a.locationDirectory = &d;           // Update file metadata
 }
 // copy (oit)
 // displaying the whole file system
 // Periodic defragmentation?
 // Need to handle space constraints
 // Taking different types of files from os or something
-// Need to implement tree structure for directory
-
 int main()
 { // Need to make user interface(Menu):
     // --Create file, directory, delete, rename,-- modify, moving, copy, taking file from os or something
     directory rootDirectory;
+
     file f1;
     char sampleText[] = "Hello thereee";
     string currentPath = "rootDirectory", displayFilesystem = "ls", makeNewFile = "new file", makeNewDirectory = "new directory", deleteObject = "delete";
